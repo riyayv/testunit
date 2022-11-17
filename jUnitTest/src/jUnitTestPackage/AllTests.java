67732f0fad069c16cab9ae9197b717c537532f92
hp <hp@10.12.63.107>
2022-11-17 15:12:28 +0530

    testjava

---
+++ jUnitTest/src/jUnitTestPackage/AllTests.java
@@ -0,0 +1,12 @@
+package jUnitTestPackage;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({ AllTests.class, testAddNumbers.class, testAddStrings.class })
+
+public class AllTests {
+
+}
---
+++ jUnitTest/src/jUnitTestPackage/testAddStrings.java
@@ -0,0 +1,17 @@
+package jUnitTestPackage;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class testAddStrings {
+
+	@Test
+	public void test() {
+		jUnitFunctions junitstring = new jUnitFunctions();
+		String result = junitstring.addStrings("hi", "there");
+		assertEquals("hithere",result);
+		
+	}
+
+}
